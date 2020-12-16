a057708d0be5e35160cfeadf27f81c5ed129339c
GrushkaS <57871440+GrushkaS@users.noreply.github.com>
2020-12-16 10:17:55 +0300

    Add files via upload

---
+++ TestDLL2/TestDLL2/PluginControl/MainUserControl.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace TestDLL2
+{
+    partial class MainUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBoxMain = new System.Windows.Forms.TextBox();
+            this.LabelMain = new System.Windows.Forms.Label();
+            this.ButtonMain = new System.Windows.Forms.Button();
+            this.LabelDescription = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TextBoxMain
+            // 
+            this.TextBoxMain.Location = new System.Drawing.Point(3, 27);
+            this.TextBoxMain.Name = "TextBoxMain";
+            this.TextBoxMain.Size = new System.Drawing.Size(268, 20);
+            this.TextBoxMain.TabIndex = 0;
+            // 
+            // LabelMain
+            // 
+            this.LabelMain.AutoSize = true;
+            this.LabelMain.Location = new System.Drawing.Point(3, 10);
+            this.LabelMain.Name = "LabelMain";
+            this.LabelMain.Size = new System.Drawing.Size(45, 13);
+            this.LabelMain.TabIndex = 1;
+            this.LabelMain.Text = "WGS84";
+            // 
+            // ButtonMain
+            // 
+            this.ButtonMain.Location = new System.Drawing.Point(325, 27);
+            this.ButtonMain.Name = "ButtonMain";
+            this.ButtonMain.Size = new System.Drawing.Size(75, 23);
+            this.ButtonMain.TabIndex = 2;
+            this.ButtonMain.Text = "Accept";
+            this.ButtonMain.UseVisualStyleBackColor = true;
+            this.ButtonMain.Click += new System.EventHandler(this.ButtonMain_Click);
+            // 
+            // LabelDescription
+            // 
+            this.LabelDescription.AutoSize = true;
+            this.LabelDescription.Location = new System.Drawing.Point(3, 59);
+            this.LabelDescription.Name = "LabelDescription";
+            this.LabelDescription.Size = new System.Drawing.Size(247, 13);
+            this.LabelDescription.TabIndex = 3;
+            this.LabelDescription.Text = "Input format: Xcoord1 Ycoord1 Xcoord2 Ycoord2...";
+            // 
+            // MainUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.LabelDescription);
+            this.Controls.Add(this.ButtonMain);
+            this.Controls.Add(this.LabelMain);
+            this.Controls.Add(this.TextBoxMain);
+            this.Name = "MainUserControl";
+            this.Size = new System.Drawing.Size(440, 210);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBoxMain;
+        private System.Windows.Forms.Label LabelMain;
+        private System.Windows.Forms.Button ButtonMain;
+        private System.Windows.Forms.Label LabelDescription;
+    }
+}
---
+++ TestDLL2/TestDLL2/PluginControl/MainUserControl.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+
+using Zulu;
+using ZuluLib;
+using ZuluCharts;
+using ZB;
+using ZuluComNetOcx;
+using ZuluOcx;
+using zulurep;
+using zuluui;
+using stdole;
+
+namespace TestDLL2
+{
+
+    [ComVisible(true)]
+    [ProgId("TestDLL2.MainUserControl")]
+    public partial class MainUserControl : UserControl
+    {
+        public MainUserControl()
+        {
+            InitializeComponent();
+        }
+
+        public void ShowForm() { }
+
+        IPluginConnector pluginConnector;
+
+        Document aDoc;
+        MapDoc Map;
+
+        Layer ActiveLayer;
+
+
+
+        public bool ConnectZuluPlugin(IPluginConnector connector) 
+        {
+            try
+            {
+                
+                pluginConnector = connector;
+                
+                aDoc = pluginConnector?.Zulu?.ActiveDocument;
+                Map = aDoc?.NativeDoc;          
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return true;
+        }
+
+        public void DisconnectZuluPlugin()
+        {           
+            pluginConnector.Unlock();
+            pluginConnector = null;
+            Map = null;
+            aDoc = null;
+        }
+
+        private void SetCRSProjection(string input)
+        {
+            ActiveLayer.SetInputCRSProjection(input);
+        }
+
+        private void GetValues(string input, out double[] Xcoord, out double[] Ycoord)
+        {
+            string[] split_text = input.Split(new char[] { ' ', ',', ';' });
+            if (split_text.Length >= 8)
+            {
+                try
+                {
+                    int n = split_text.Length / 2;
+                    Xcoord = new double[n];
+                    Ycoord = new double[n];
+                    int x = 0;
+                    for (int i = 0; i < n; i++)
+                    {
+                        Xcoord[i] = double.Parse(split_text[x]);
+                        Ycoord[i] = double.Parse(split_text[x + 1]);
+                        x += 2;
+                    }
+
+
+                    #region debugging
+                    /*
+                    string what = null;
+                    string what2 = null;
+                    for(int i = 0; i < split_text.Length; i++)
+                    {
+                        what2 += split_text[i] + " ";
+                    }
+
+                    for (int i = 0; i < Xcoord.Length; i++)
+                    {                 
+                        what += Xcoord[i].ToString() + ";" + Ycoord[i].ToString() + ". "; 
+                    }
+
+                    MessageBox.Show(what2, "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show(what, "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);*/
+                    #endregion
+                }
+                catch
+                {
+                    Xcoord = null;
+                    Ycoord = null;
+                }
+            }
+            else
+            {
+                Xcoord = null;
+                Ycoord = null;
+            }
+
+        }
+
+        private void Draw(double[] Xcoord, double[] Ycoord)
+        {
+            int id = ActiveLayer.GetLargestID();
+            //int id = ActiveLayer.Elements.Count;
+            //int id = 4;
+            //int id = ActiveLayer.ElementKeys[ActiveLayer.ElementKeys.Count];
+            //int id = ActiveLayer.Selection.ElementKeys.Count;
+            //MessageBox.Show("Selected id = " + id.ToString(), "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);         
+            
+            PolyLine pLine = ActiveLayer.Elements.GetElement(id).PolyLine;
+            //MessageBox.Show("Selected element = " + ActiveLayer.Elements.GetElement(id).Key.ToString(), "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            pLine.Fill(4, ref Xcoord[0], ref Ycoord[0]);
+            pLine.AddPoint(Xcoord[0], Ycoord[0]);
+
+            //ActiveLayer.Elements.GetElement(ActiveLayer.ElementKeys[id]).SetPolyLine(pLine);
+
+            #region debug
+            MessageBox.Show(ActiveLayer.Elements.GetElement(ActiveLayer.ElementKeys[1]).SetPolyLine(pLine).ToString(), "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //MessageBox.Show(el.SetPolyLine(pLine).ToString(), "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            #endregion
+        }
+
+
+        private void ButtonMain_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double[] Xcoord, Ycoord;
+                GetValues(TextBoxMain.Text, out Xcoord, out Ycoord);
+
+                if(Xcoord == null) MessageBox.Show("Incorrect input data", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                {
+                    ActiveLayer = Map?.Layers?.Active;
+                    SetCRSProjection("EPSG:32601");
+
+                    Draw(Xcoord, Ycoord);
+                }
+                
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show(ex.Message, "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+    }
+}
